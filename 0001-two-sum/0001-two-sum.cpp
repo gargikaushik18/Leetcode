2bb33531783d36747275794371d7c9e8fbf83100
Gargi Kaushik <152196374+gargikaushik18@users.noreply.github.com>
2024-05-09 15:07:29 +0530

    Time: 8 ms (79.23%), Space: 14.1 MB (17.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,34 @@
+
+
+// class Solution {
+// public:
+
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int,int>current;
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size();i++){
+//             int complement=target-nums[i];
+//             if(current.find(complement)!=current.end()){
+//             return {current[complement],i};
+//             }
+//             current[nums[i]]=i;
+//     }
+//     return {};
+// }
+    
+// };
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        unordered_map<int, int> numIndexMap; 
+    vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> numIndexMap; // Map to store number-index pairs
         for (int i = 0; i < nums.size(); ++i) {
             int complement = target - nums[i];
             auto it = numIndexMap.find(complement);
             if (it != numIndexMap.end()) {
-                return {it->second, i}; 
+                return {it->second, i}; // Return original indices
             }
-            numIndexMap[nums[i]] = i; 
+            numIndexMap[nums[i]] = i; // Store current number and its index
         }
-        return {};
+        return {}; // Return empty vector if no solution is found
     }
 };
