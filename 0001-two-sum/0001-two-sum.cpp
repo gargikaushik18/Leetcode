e464d006e723787104db8bd90b469ee8226f8920
Gargi Kaushik <152196374+gargikaushik18@users.noreply.github.com>
2024-05-13 13:54:58 +0530

    Time: 106 ms (16.73%), Space: 12.7 MB (54.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,34 +1,29 @@
-
-
-// class Solution {
-// public:
-
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         unordered_map<int,int>current;
-//         sort(nums.begin(),nums.end());
-//         for(int i=0;i<nums.size();i++){
-//             int complement=target-nums[i];
-//             if(current.find(complement)!=current.end()){
-//             return {current[complement],i};
-//             }
-//             current[nums[i]]=i;
-//     }
-//     return {};
-// }
-    
-// };
+#include<vector>
 class Solution {
 public:
+    // vector<int> twoSum(std::vector<int>& nums, int target) {
+    //     std::unordered_map<int, int> numIndexMap; // Map to store number-index pairs
+    //     for (int i = 0; i < nums.size(); ++i) {
+    //         int complement = target - nums[i];
+    //         auto it = numIndexMap.find(complement);
+    //         if (it != numIndexMap.end()) {
+    //             return {it->second, i}; // Return original indices
+    //         }
+    //         numIndexMap[nums[i]] = i; // Store current number and its index
+    //     }
+    //     return {}; // Return empty vector if no solution is found
+    // }
     vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::unordered_map<int, int> numIndexMap; // Map to store number-index pairs
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
-            auto it = numIndexMap.find(complement);
-            if (it != numIndexMap.end()) {
-                return {it->second, i}; // Return original indices
+        vector<int>arr;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(target==nums[i]+nums[j]){
+                    arr.push_back(i);
+                    arr.push_back(j);
+                    return arr;
+                }
             }
-            numIndexMap[nums[i]] = i; // Store current number and its index
         }
-        return {}; // Return empty vector if no solution is found
-    }
+            return arr;
+        }
 };
